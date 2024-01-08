8a102dfe9e2fcd2943b9b284bde655c59034d97f
siwail <58031651+siwail@users.noreply.github.com>
2024-01-08 10:51:44 +0300

    First commit

---
+++ MemeSlayer/desktop/src/com/meme/slayer/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.meme.slayer;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.meme.slayer.Main;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Meme Slayer");
+		new Lwjgl3Application(new Main(), config);
+	}
+}
